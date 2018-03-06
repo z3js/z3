e22273a6545f2c70f2a711d89c08ffe54dcab099
Tao.zhang <litson.zhang@gmail.com>
2018-03-06 14:30:31 +0800

    updates

--- command/init.js
+++ command/init.js
@@ -1,77 +1,114 @@
 /**
  * @file
- * @author
  */
 
-/* global module, __dirname */
+let logger = require( '../lib/logger' );
+let {
+        checkTemplate,
+        getScaffold,
+        readFile
+    }      = require( '../lib/util' );
 
-'use strict';
-
-let process = require( 'process' );
-let path    = require( 'path' );
-
-let co     = require( 'co' );
-let prompt = require( 'co-prompt' );
-let chalk  = require( 'chalk' );
-let ora    = require( 'ora' );
-
-let download = require( '../lib/download' );
-let build    = require( '../lib/build' );
-let util     = require( '../lib/util' );
-let logger   = require( '../lib/logger' );
-
-let pkg = require( '../package.json' );
+let templatePath;
 
 module.exports = function ( templateName ) {
 
-    if ( typeof templateName !== 'string' ) {
-        return templateName.help();
-    }
+    let pkg            = require( '../package.json' );
+    let download       = require( '../lib/download' );
+    let {findTemplate} = require( '../lib/util' );
 
-    const spinner = ora( 'downloading template' );
-    spinner.start();
-    return download( pkg.z3conf.template, function ( scaffold, tempPath ) {
-        spinner.stop();
-        deliver( scaffold, {
-            tempPath    : tempPath,
-            templateName: templateName
-        } );
-    } );
+    download( pkg.z3conf.template )
+        .then( res => findTemplate( templateName, res.path ) )
+        .then( setGlobalTemplatePath )
+        .then( path => checkTemplate( templateName, path ) )
+        .then( runMeta )
+        .then( build )
+        .catch( logger.fatal );
 };
 
-function deliver( scaffold, options ) {
+/**
+ * 缓存一下已找到的模板路径
+ */
+function setGlobalTemplatePath( path ) {
+    templatePath = path;
+    return templatePath;
+}
 
-    let {tempPath, templateName} = options;
+/**
+ * 运行模板的meta.js
+ */
+function runMeta( files ) {
+    let meta = require( getMetaPath() )();
+    return {
+        files: files,
+        meta : meta
+    };
+}
 
-    let cwd      = process.cwd();
-    let fromPath = path.join( tempPath, 'templates', templateName );
+function getMetaPath() {
+    return require( 'path' ).join( templatePath, 'meta.js' );
+}
 
-    let files = scaffold.util.find( fromPath );
+function build( options ) {
 
-    if ( files.length === 0 ) {
-        return logger.fatal( 'Failed to download repo [ ' + templateName + ' ]' );
-    }
+    let co      = require( 'co' );
+    let prompt  = require( 'co-prompt' );
+    let process = require( 'process' );
+
+    let getAskDescription = require( '../lib/util' ).getAskDescription;
+
+    let {files, meta} = options;
 
     co( function* () {
-        let projectName = util.getLocalDirName();
-        let namespace   = yield prompt( util.getAskDescription( 'Project Name', projectName ) );
+        let data    = meta.data;
+        let prompts = meta.prompts;
 
-        if ( namespace === '' ) {
-            namespace = projectName;
+        for ( let key in prompts ) {
+            let temp  = prompts[key];
+            data[key] = yield prompt( getAskDescription( temp.message, temp.default ) );
+
+            if ( data[key] === '' ) {
+                data[key] = temp.default;
+            }
         }
 
         process.stdin.pause();
 
-        files.forEach( val => {
-            val = val.replace( fromPath, '' ).replace( /^\\/g, '' );
-            console.log( chalk.yellow( '  Installing ' ) + val );
-        } );
+        // 文件移转
+        getScaffold().util.move( templatePath, process.cwd() );
 
-        scaffold.util.move(
-            fromPath,
-            cwd
-        );
-
-        build( namespace, cwd );
+        replacing( files, meta );
     } );
+}
+
+function replacing( files, meta ) {
+    let PATH = require( 'path' );
+    let etpl = require( 'etpl' );
+    let fs   = require( 'fs' );
+    let cwd  = require( 'process' ).cwd();
+
+    // 获取问题结果
+    files.forEach( path => {
+        let fileName = path.replace( templatePath, '' ).replace( /^\\/g, '' );
+
+        path = PATH.resolve( cwd + '/' + fileName );
+
+        let file   = readFile( path );
+        let render = etpl.compile( file );
+
+        fs.writeFileSync(
+            path,
+            render( meta.data ),
+            {encoding: 'utf8', flag: 'w'} );
+        logger.warn( '  Installing ' + fileName );
+    } );
+
+    let $scaffold = getScaffold();
+
+    // 删掉忽略列表文件
+    meta.ignore.forEach( file => {
+        $scaffold.util.del( PATH.resolve( cwd + file ) );
+        logger.warn( '  Removing ignore file ' + file );
+    } );
+
 }
--- command/list.js
+++ command/list.js
@@ -1,43 +1,35 @@
-/**
- * @file
- * @author
- */
-
-/* global module, __dirname */
-
-'use strict';
-
-var pkg      = require( '../package.json' );
-var download = require( '../lib/download' );
-var chalk    = require( 'chalk' );
-var each     = require( '../lib/util' ).each;
-
 module.exports = function ( options ) {
 
-    if ( typeof  options != 'object' ) {
+    if ( typeof  options !== 'object' ) {
         return console.log( 'Unknown option [ %s ]!', options );
     }
 
-    var id      = pkg.z3conf.template;
-    var snippet = 'templates';
+    let download = require( '../lib/download' );
+    let pkg      = require( '../package.json' );
+    let each     = require( '../lib/util' ).each;
+    let chalk    = require( 'chalk' );
+    let id       = pkg.z3conf.template;
+    let snippet  = 'templates';
 
     if ( options.components ) {
         id      = pkg.z3conf.component;
         snippet = 'components';
     }
 
-    download( id, function ( scaffold, tempPath, pkg ) {
-        console.log();
-        console.log( '  Available official %s:', snippet );
-        console.log();
-        each(
-            pkg.data, function ( repo ) {
-                console.log(
-                    '  ' + chalk.yellow( '★' ) +
-                    '  ' + chalk.blue( repo.name ) +
-                    ' - ' + (repo.description || '') );
-            }
-        );
-        console.log();
-    } );
+    download( id )
+        .then( res => {
+            let {pkg} = res;
+            console.log();
+            console.log( '  Available official %s:', snippet );
+            console.log();
+            each(
+                pkg.data, function ( repo ) {
+                    console.log(
+                        '  ' + chalk.yellow( '★' ) +
+                        '  ' + chalk.blue( repo.name ) +
+                        ' - ' + (repo.description || '') );
+                }
+            );
+            console.log();
+        } );
 };
