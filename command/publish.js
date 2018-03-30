25ec49410f8805db5efe9b2d4e734f4804c672c6
Tao.zhang <litson.zhang@gmail.com>
2018-03-30 17:52:09 +0800

    修复不同系统下分隔符引发的bug

--- command/publish.js
+++ command/publish.js
@@ -1,4 +1,4 @@
-module.exports = function () {
+module.exports = function ( componentName ) {
     let getRc    = require( '../lib/config' ).get;
     let download = require( '../lib/download' );
     let pkg      = require( '../package.json' );
@@ -51,13 +51,22 @@ module.exports = function () {
         function build( file, type ) {
             let fileName     = file.replace( process.cwd(), '' );
             let localDirName = getLocalDirName();
+            let Path         = require( 'path' );
+
+            let name = (componentName || localDirName);
+
+            let filePath = ['', 'components', name,
+                            fileName]
+                .map( i => i.replace( Path.sep, '' ) )
+                .join( Path.sep );
+
             // 尝试创建
             req( buildOptions(
                 `${type} ${fileName}`,
                 {
                     content  : $s.util.read( file ),
                     action   : type,
-                    file_path: `/components/${localDirName}${fileName}`
+                    file_path: filePath
                 }
             ) ).then( data => {
                     logger.warn( ` ${data.author_name}<${data.author_email}> ${type} ${fileName} success` );
@@ -65,9 +74,9 @@ module.exports = function () {
                     current++;
                     if ( current === max ) {
                         logger.success( ` 
-                        Publish "${localDirName}" success, to get started:
+                        Publish "${name}" success, to get started:
                         
-                            z3 install ${localDirName}` )
+                            z3 install ${name}` )
                     }
                 }
                 // 失败则尝试更新
